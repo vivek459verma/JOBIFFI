98988166d7d47f95d235c802d23e16a1e1536d85
priya19tiwari <priyatiwari.srv@gmail.com>
2026-03-01 19:36:56 +0530

    Added Trust & Safety page with routing

--- Frontend/src/Components/Footer/footer.tsx
+++ Frontend/src/Components/Footer/footer.tsx
@@ -5,8 +5,9 @@ import logo from "../../assets/media/New_Brand_logo_-_16060-removebg.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-white mt-10 ">
-      <div className="max-w-7xl  py-10 mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
+    <footer className="bg-white mt-10">
+      <div className="max-w-7xl py-10 mx-auto px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
+        
         {/* Column 1 */}
         <div>
           <a href="/">
@@ -21,14 +22,13 @@ const Footer = () => {
                 className="h-7 w-7 object-contain"
               />
             </a>
-            <a href="https://www.instagram.com/jobiffi?igsh=dnMwNWozYnBuODc2">
+            <a href="https://www.instagram.com/jobiffi">
               <img
-                src="https://img.freepik.com/premium-psd/instagram-logo-social-media-icon_705838-13489.jpg?semt=ais_hybrid&w=740&q=80"
+                src="https://img.freepik.com/premium-psd/instagram-logo-social-media-icon_705838-13489.jpg"
                 alt="instagram"
                 className="h-7 w-7 object-contain"
               />
             </a>
-
             <a href="https://www.linkedin.com/company/jobiffi/">
               <img
                 src="https://e7.pngegg.com/pngimages/524/809/png-clipart-computer-icons-resume-linkedin-logo-job-hunting-others-blue-angle-thumbnail.png"
@@ -38,7 +38,7 @@ const Footer = () => {
             </a>
             <a href="https://x.com/jobiffidotcom">
               <img
-                src="https://img.freepik.com/free-vector/new-2023-twitter-logo-x-icon-design_1017-45418.jpg?semt=ais_hybrid&w=740&q=80"
+                src="https://img.freepik.com/free-vector/new-2023-twitter-logo-x-icon-design_1017-45418.jpg"
                 alt="twitter"
                 className="h-5 w-5 ml-1 object-contain"
               />
@@ -49,17 +49,10 @@ const Footer = () => {
         {/* Column 2 */}
         <div>
           <ul className="mt-3 space-y-2 text-gray-600">
-            <a href="/" className="hover:text-blue-900 cursor-pointer block">
-              Home
-            </a>
-            <a
-              href="/about-us"
-              rel="noopener noreferrer"
-              className="hover:text-blue-900 cursor-pointer block"
-            >
+            <a href="/" className="hover:text-blue-900 block">Home</a>
+            <a href="/about-us" className="hover:text-blue-900 block">
               About us
             </a>
-            {/* <li className="hover:text-blue-900 cursor-pointer">About us</li> */}
             <li className="hover:text-blue-900 cursor-pointer">Careers</li>
             <li className="hover:text-blue-900 cursor-pointer">Hiring hub</li>
             <li className="hover:text-blue-900 cursor-pointer">Sitemap</li>
@@ -69,12 +62,8 @@ const Footer = () => {
         {/* Column 3 */}
         <div>
           <ul className="mt-3 space-y-2 text-gray-600">
-<<<<<<< HEAD
-          
-=======
->>>>>>> 718fc3229d5ecd92c8796784d3bd26ffc4466659
             <li className="hover:text-blue-900 cursor-pointer">Jobs</li>
-            <li className="hover:text-blue-900 cursor-pointer">Compaines</li>
+            <li className="hover:text-blue-900 cursor-pointer">Companies</li>
             <li className="hover:text-blue-900 cursor-pointer">Services</li>
             <li className="hover:text-blue-900 cursor-pointer">Resources</li>
             <li className="hover:text-blue-900 cursor-pointer">Blogs</li>
@@ -82,89 +71,47 @@ const Footer = () => {
         </div>
 
         {/* Column 4 */}
-        {/* Column 4 */}
-        {/* Column 4 */}
         <div>
           <ul className="mt-3 space-y-2 text-gray-600">
             <li className="hover:text-blue-900 cursor-pointer">Help Center</li>
 
-            <a
-              href="/privacy-policy"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-blue-900 cursor-pointer block"
-            >
+            <a href="/privacy-policy" target="_blank" className="hover:text-blue-900 block">
               Privacy Policy
             </a>
 
-            <a
-              href="/terms-conditions"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-blue-900 cursor-pointer block"
-            >
-              Terms & conditions
+            <a href="/terms-conditions" target="_blank" className="hover:text-blue-900 block">
+              Terms & Conditions
             </a>
 
-<<<<<<< HEAD
-           <a
-  href="/fraud-alert"
-  target="_blank"
-  rel="noopener noreferrer"
-  className="hover:text-blue-900 cursor-pointer block"
->
-  Fraud Alert
-</a>
+            <a href="/fraud-alert" target="_blank" className="hover:text-blue-900 block">
+              Fraud Alert
+            </a>
 
-
-           <a
-  href="/trust-safety"
-  target="_blank"
-  rel="noopener noreferrer"
-  className="hover:text-blue-900 cursor-pointer block"
->
-  Trust & Safety
-</a>
-
-
-=======
-            <li className="hover:text-blue-900 cursor-pointer">Fraud alert</li>
-
-            <li className="hover:text-blue-900 cursor-pointer">
+            <a href="/trust-safety" target="_blank" className="hover:text-blue-900 block">
               Trust & Safety
-            </li>
->>>>>>> 718fc3229d5ecd92c8796784d3bd26ffc4466659
+            </a>
           </ul>
         </div>
 
         {/* Column 5 */}
         <div>
-          <h6 className="text-black font-semibold mt-3">Get the App today</h6>
+          <h6 className="font-semibold mt-3">Get the App today</h6>
           <p className="text-gray-600 text-sm mt-2">
-            Trusted by 5,000 + Job seekers
+            Trusted by 5,000+ Job seekers
           </p>
           <div className="flex gap-3 mt-4">
             <img src={goggle} alt="android" className="h-10" />
             <img src={play} alt="playstore" className="h-10" />
-            {/* <img
-              src="https://static.naukimg.com/s/0/0/i/new-homepage/android-app_v1.png"
-              alt="android"
-              className="h-10"
-            />
-            <img
-              src="https://static.naukimg.com/s/0/0/i/new-homepage/ios-app_v1.png"
-              alt="ios"
-              className="h-10"
-            /> */}
           </div>
         </div>
+
       </div>
-      <div className="border border-r border-gray-200"></div>
+
+      <div className="border border-gray-200"></div>
+
       <div className="text-center mt-4 mb-4 text-gray-700">
         <h6 className="flex justify-center gap-2">
-          All right reserved{" "}
-          <AiOutlineCopyright className="text-center  mt-1" /> 2026 Jobiffi (P)
-          Ltd.
+          All rights reserved <AiOutlineCopyright className="mt-1" /> 2026 Jobiffi (P) Ltd.
         </h6>
       </div>
     </footer>
